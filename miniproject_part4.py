caa5c566ad08696b387ed3c66d93b5245d37c299
Shreyas R.K <74357270+ShreyasRkk@users.noreply.github.com>
2024-01-20 15:38:55 -0800

    Update miniproject_part4.py

--- miniproject_part4.py
+++ miniproject_part4.py
@@ -47,7 +47,8 @@ def get_model_id_gdrive(model_type):
         word_index_id = "13qMXs3-oB9C6kfSRMwbAtzda9xuAUtt8"
         embeddings_id = "1-RXcfBvWyE-Av3ZHLcyJVsps0RYRRr_2"
     elif model_type == "50d":
-        embeddings_id = "1DBaVpJsitQ1qxtUvV1Kz7ThDc3az16kZ"
+        #embeddings_id = "1DBaVpJsitQ1qxtUvV1Kz7ThDc3az16kZ"
+        embeddings_id = "1kNbE4RIDW9vDuw8In1rDP7Zk3FSJI"
         word_index_id = "1rB4ksHyHZ9skes-fJHMa2Z8J1Qa7awQ9"
     elif model_type == "100d":
         word_index_id = "1-oWV0LqG3fmrozRZ7WB1jzeTJHRUI3mq"
